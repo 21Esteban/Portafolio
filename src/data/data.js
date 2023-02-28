0907a6ee4c99546a8cc191a498e1067b629222f6
21Esteban <jelc200521@gmail.com>
2023-02-28 15:40:35 -0500

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,103 @@
+import JavaScripImg from "../assets/img/js.png";
+import nodejs from "../assets/img/nodejs.png";
+import React from "../assets/img/react.png";
+import redux from "../assets/img/redux.png";
+import express from "../assets/img/express.png";
+import fastify from "../assets/img/fastify.png";
+import html from "../assets/img/html.png";
+import css from "../assets/img/css.png";
+import java from "../assets/img/java.png";
+import springboot from "../assets/img/springboot.png";
+import MongoDb from "../assets/img/mongodb.png";
+import MySql from "../assets/img/mysql.png";
+import bootstrap from "../assets/img/bootstrap-logo.png";
+import native from "../assets/img/reactNative.png";
+import git from "../assets/img/git1.png";
+
+//Repos
+
+import imageRepo from "../assets/img/Captura de pantalla 2023-02-28 032531.png"
+
+export const technologies = [
+  {
+    name: "JavaScript",
+    img: JavaScripImg,
+  },
+  {
+    name: "NodeJs",
+    img: nodejs,
+  },
+  {
+    name: "React",
+    img: React,
+  },
+  {
+    name: "Redux toolkit",
+    img: redux,
+  },
+  {
+    name: "Express",
+    img: express,
+  },
+  {
+    name: "Fastify",
+    img: fastify,
+  },
+  {
+    name: "Html",
+    img: html,
+  },
+  {
+    name: "Css",
+    img: css,
+  },
+  {
+    name: "Java",
+    img: java,
+  },
+  {
+    name: "SpringBoot",
+    img: springboot,
+  },
+  {
+    name: "MongoDb",
+    img: MongoDb,
+  },
+  {
+    name: "MySql",
+    img: MySql,
+  },
+  {
+    name: "Bootstrap",
+    img: bootstrap,
+  },
+  {
+    name: "React Native",
+    img: native,
+  },
+  {
+    name: "Git",
+    img: git,
+  },
+];
+
+export const data = [
+  {
+    proyect:"Notes Redux",
+    link:"https://appdenotes.netlify.app/",
+    repo:"https://github.com/21Esteban",
+    img:imageRepo
+  },
+  {
+    proyect:"Notes Redux",
+    link:"https://appdenotes.netlify.app/",
+    repo:"https://github.com/21Esteban",
+    img:imageRepo
+  },
+  {
+    proyect:"Notes Redux",
+    link:"https://appdenotes.netlify.app/",
+    repo:"https://github.com/21Esteban",
+    img:imageRepo
+  }
+];
